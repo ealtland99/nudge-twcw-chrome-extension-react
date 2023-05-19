40ac75250fc4dbadc8ef3e1e5557901c0b1219c5
ealtland99 <ealtland99@gmail.com>
2023-05-18 20:03:26 -0400

    hid browser action stuff in background.js for now

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -4,3 +4,16 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
 
+
+
+// // Called when the user clicks on the browser action
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//   // Send a message to the active tab
+//   chrome.tabs.query({ active: true, currentWindow: true },
+//     function (tabs) {
+//       var activeTab = tabs[0];
+//       chrome.tabs.sendMessage(activeTab.id,
+//         { "message": "clicked_browser_action" }
+//       );
+//     });
+// });
